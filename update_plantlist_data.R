1f8631accf9b174fcf2fd6c244f6f3d95b8e5fa2
Jinlong <jinlongzhang01@gmail.com>
2017-04-25 22:54:28 +0800

    revised the data

--- update_plantlist_data.R
+++ update_plantlist_data.R
@@ -1,7 +1,14 @@
-######## Data source for the herblabel package ################
-######## if any update is needed, edit the following files and regenerate the rda files.
+getScriptPath <- function(){
+    cmd.args <- commandArgs()
+    m <- regexpr("(?<=^--file=).+", cmd.args, perl=TRUE)
+    script.dir <- dirname(regmatches(cmd.args, m))
+    if(length(script.dir) == 0) stop("can't determine script dir: please call the script with Rscript")
+    if(length(script.dir) > 1) stop("can't determine script dir: more than one '--file' argument detected")
+    return(script.dir)
+}
 
-setwd("C:\\Jinlong\\github packages\\plantlist_data")
+res <- getScriptPath()
+setwd(res)
 library(openxlsx)
 rm(list = ls())
 cnplants <- read.xlsx("plants_master.xlsx")
